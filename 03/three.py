86627ce5a985cb38e48b8b345cabc11e34ee3723
105624033 <105624033@RESPLN-PC71>
2025-10-15 08:46:43 +0330

    three

---
+++ 03/three.py
@@ -0,0 +1,21 @@
+#Camel case Starts with Capitol letter 
+CamelCase = "Test"
+
+#Case Sensitive 
+camelCase = "test2"
+# None = "test"
+
+prt = str(camelCase.__len__)
+
+print(prt)
+
+
+holder = int("10") + int("10")
+print(holder + 10)
+
+
+print(10 + float("10"))
+
+name = "Jenny"
+
+print(10 + int(name))
