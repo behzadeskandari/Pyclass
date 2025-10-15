01e6d169a34bbb6010124651cd08a0fe1fcab1a1
105624033 <105624033@RESPLN-PC71>
2025-10-15 07:59:58 +0330

    cmt

--- 01/first.py
+++ 01/first.py
@@ -3,3 +3,13 @@
 
 print('hello')
 
+print('hello Line \n Hello dude and dudet')
+
+output = input("Whats is your Name give me an answer")
+nums = [15,12,13,16,98,1999]
+
+print(output)
+print(any(nums))
+
+print("Hello User" + " "+ output + " " + input("What age you have"))
+#comment :TODO Change Later 
