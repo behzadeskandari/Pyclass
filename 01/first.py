1e3d4b9b023a34ea5d86e20d21f7fdfaf0da40c0
105624033 <105624033@RESPLN-PC71>
2025-10-15 07:59:58 +0330

    cmt

--- 01/first.py
+++ 01/first.py
@@ -3,3 +3,52 @@
 
 print('hello')
 
+print('hello Line \n Hello dude and dudet')
+
+output = input("Whats is your Name give me an answer")
+nums = [15,12,13,16,98,1999]
+
+print(output)
+print(any(nums))
+
+print("Hello User" + " "+ output + " " + input("What age you have"))
+#comment :TODO Change Later 
+
+length = len(output)
+
+print("the length of the output is", length)
+
+a = 1
+b = "behzad"
+c = 'b'
+
+print(a)
+print(b + c)
+
+
+
+a = input("enter value of a = ")
+b = input("enter value of b = ")
+
+temp = a 
+a = b 
+b = temp
+
+
+print("aaaaa",a)
+print("bbbb",b)
+print("temp",temp)
+
+name = 1
+_name = 2
+
+# 0name
+#pascal case
+my_name_variable = 3
+
+#Camel case Starts with Capitol letter 
+CamelCase = "Test"
+
+#Case Sensitive 
+camelCase = "test2"
+# None = "test"
